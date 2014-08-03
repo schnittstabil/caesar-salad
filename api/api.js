1f2d5f1dd37fc29aae9a897ea801992efdd9a8fe
Michael Mayer <michael@schnittstabil.de>
2014-08-03 20:01:49 +0200

    Initial commit

---
+++ api/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Caesar",
+        "CaesarSalad",
+        "Password",
+        "ROT13",
+        "ROT18",
+        "ROT47",
+        "ROT5",
+        "SubstitutionCipher",
+        "Vigenere"
+    ],
+    "modules": [
+        "caesar-salad"
+    ],
+    "allModules": [
+        {
+            "displayName": "caesar-salad",
+            "name": "caesar-salad"
+        }
+    ]
+} };
+});
